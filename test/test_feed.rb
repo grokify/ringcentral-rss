8f1fcc1eecc4e9f692b0031f42de20146d6994f5
John Wang <johncwang@gmail.com>
2016-12-24 21:31:03 +0000

    update style

--- test/test_feed.rb
+++ test/test_feed.rb
@@ -4,26 +4,27 @@ require 'nokogiri'
 require 'ringcentral-rss'
 
 class RingCentralRSSFeedTest < Test::Unit::TestCase
+  TEST_MESSAGE = {
+    'to' => [{ 'phoneNumber' => '+16505551212' }],
+    'from' => { 'phoneNumber' => '+14155551212' },
+    'subject' => 'hello world!',
+    'direction' => 'Inbound',
+    'type' => 'SMS',
+    'lastModifiedTime' => '2015-01-01T00:00:00+0000'
+  }.freeze
+
+  TEST_HEADERS = { 'date' => 'Wed, 09 Nov 2016 06:46:16 GMT' }.freeze
+
+  TEST_XML_TIME = '2016-11-09T06:46:16+00:00'.freeze
+
   def test_main
-    headers = { 'date' => 'Wed, 09 Nov 2016 06:46:16 GMT' }
-    xml_time = '2016-11-09T06:46:16+00:00'
-
-    message = {
-      'to' => [{ 'phoneNumber' => '+16505551212' }],
-      'from' => { 'phoneNumber' => '+14155551212' },
-      'subject' => 'hello world!',
-      'direction' => 'Inbound',
-      'type' => 'SMS',
-      'lastModifiedTime' => '2015-01-01T00:00:00+0000'
-    }
-
     body = {
       'uri' => 'https://platform.devtest.ringcentral.com/restapi/v1.0/account/~/extension/~/message-store',
-      'records' => [message]
+      'records' => [TEST_MESSAGE]
     }
 
     stubs = Faraday::Adapter::Test::Stubs.new do |stub|
-      stub.get('/message-store') { [200, headers, body] }
+      stub.get('/message-store') { [200, TEST_HEADERS, body] }
     end
 
     test = Faraday.new do |builder|
@@ -51,11 +52,11 @@ class RingCentralRSSFeedTest < Test::Unit::TestCase
     end
 
     doc.xpath('//feed/updated').each do |el|
-      assert_equal xml_time, el.text
+      assert_equal TEST_XML_TIME, el.text
     end
 
     doc.xpath('//feed/entry/summary').each do |el|
-      assert_equal message['subject'], el.text
+      assert_equal TEST_MESSAGE['subject'], el.text
     end
 
     feed = RingCentral::RSS::AtomFeed.new res, feed_name: 'Awesome Feed'
